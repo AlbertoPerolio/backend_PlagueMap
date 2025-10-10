d4b77a32581a7bae09e5f1e63527271e6c9fb939
AlbertoPerolio <albertoperolio0319@gmail.com>
2025-10-10 11:42:12 -0300

    .

--- src/modules/auth/controller.js
+++ src/modules/auth/controller.js
@@ -1,27 +1,38 @@
-import bcrypt from "bcrypt";
-import RegInf from "../../models/RegInf.js";
-import { assignToken } from "../../authlog/index.js";
-
-export default function authController() {
-  async function login(identifier, password) {
-    let user = await RegInf.findOne({ where: { user: identifier } });
-    if (!user) user = await RegInf.findOne({ where: { email: identifier } });
-
-    if (!user) {
-      const error = new Error("El usuario no existe");
-      error.statusCode = 404;
-      throw error;
-    }
-
-    const isValid = await bcrypt.compare(password, user.password);
-    if (!isValid) {
-      const error = new Error("Datos incorrectos");
-      error.statusCode = 401;
-      throw error;
-    }
-
-    return assignToken(user.toJSON());
+async function login(identifier, password, res) {
+  let user = await RegInf.findOne({ where: { user: identifier } });
+  if (!user) user = await RegInf.findOne({ where: { email: identifier } });
+  if (!user) {
+    const error = new Error("El usuario no existe");
+    error.statusCode = 404;
+    throw error;
   }
 
-  return { login };
+  const isValid = await bcrypt.compare(password, user.password);
+  if (!isValid) {
+    const error = new Error("Datos incorrectos");
+    error.statusCode = 401;
+    throw error;
+  }
+
+  const token = assignToken({
+    id_reg: user.id_reg,
+    role: user.role,
+    name: user.name,
+  });
+
+  // Guardar token en cookie httpOnly
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 días
+    sameSite: "lax",
+  });
+
+  return {
+    user: {
+      id_reg: user.id_reg,
+      name: user.name,
+      role: user.role,
+    },
+  };
 }
